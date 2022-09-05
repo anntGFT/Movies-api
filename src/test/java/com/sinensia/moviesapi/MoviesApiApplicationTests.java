88f3d5d576c8c5d13ed3e217125693bdcfbd6213
annt <annt@gft.com>
2022-09-05 10:08:16 +0000

    Primer commit

--- src/test/java/com/sinensia/moviesapi/MoviesApiApplicationTests.java
+++ src/test/java/com/sinensia/moviesapi/MoviesApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MoviesApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
