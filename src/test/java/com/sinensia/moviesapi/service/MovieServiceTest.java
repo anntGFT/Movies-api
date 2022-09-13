f4ab19447fccda1adb1bf2482f229cae0a03848b
annt <annt@gft.com>
2022-09-13 09:16:51 +0000

    Arreglando cosas

---
+++ src/test/java/com/sinensia/moviesapi/service/MovieServiceTest.java
@@ -0,0 +1,44 @@
+package com.sinensia.moviesapi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+
+    @Test
+    void getAllGenres() {
+    }
+
+    @Test
+    void getPopular() {
+    }
+
+    @Test
+    void getTopRated() {
+    }
+
+    @Test
+    void getMovieById() {
+    }
+
+    @Test
+    void getredits() {
+    }
+
+    @Test
+    void getAllImages() {
+    }
+
+    @Test
+    void getKeywords() {
+    }
+
+    @Test
+    void getRecommendations() {
+    }
+
+    @Test
+    void getSimilarMovies() {
+    }
+}
